6409838b425698360f5aebc269151a4100c347db
BIKRAM GORAI <bikram77620@gmail.com>
2025-09-10 15:35:00 +0530

    template 5

---
+++ DAY 4/template5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+
+T squares(T x)
+{
+    T result = x + 10;
+    return result;
+}
+
+template <>
+string squares<string>(string s)
+{
+    return "not applicable for strings";
+}
+
+int main()
+{
+    cout << squares<int>(5) << endl;
+    cout << squares<double>(4.5) << endl;
+    cout << squares<string>("hello world") << endl;
+    return 0;
+}
