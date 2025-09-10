ff6d963103b5a81a0b3b5c8cab705e8a774f339b
BIKRAM GORAI <bikram77620@gmail.com>
2025-09-10 16:46:15 +0530

    modified bubble sort

--- DAY 4/bubble_sort1.cpp
+++ DAY 4/bubble_sort1.cpp
@@ -5,7 +5,7 @@ void bubblesort(int arr[],int n)
 {
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < n-i-1; j++)
         {
             if (arr[j] > arr[j + 1])
             {
