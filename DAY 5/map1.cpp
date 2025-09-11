41de5a24b635a39b00cc0c546c14771d2dc90466
BIKRAM GORAI <bikram77620@gmail.com>
2025-09-11 14:43:06 +0530

    map

---
+++ DAY 5/map1.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+int main()
+{
+    map<string,int> ages;
+    ages["Amit"]=23;
+    ages["Ayush"]=25;
+    ages["Baidya"]=35;
+    ages["Bikram"]=33;
+    ages.insert({"gourab",34});
+
+    cout<<"age of subham: "<<ages["Bikram"];
+
+    for(auto& pair : ages)
+    {
+        cout<<pair.first<<"  is  "<<pair.second<<"  years old "<<endl;
+    }
+
+    if (ages.count("Amit"))
+    {
+        cout<<"Amit is in the map "<<endl;
+    }
+
+    ages.erase("Ayush");
+    for(auto& pair : ages)
+    {
+        cout<<pair.first<<"  is  "<<pair.second<<"  years old "<<endl;
+    }
+    return 0;
+}
