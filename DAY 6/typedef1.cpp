e3caba1c333e0d8b2ad56052e8482b26d236c33c
BIKRAM GORAI <bikram77620@gmail.com>
2025-09-12 16:35:33 +0530

    new file:   DAY 6/ENUM1.CPP 	new file:   DAY 6/typedef1.cpp

---
+++ DAY 6/typedef1.cpp
@@ -0,0 +1,14 @@
+//typedef in c++
+#include<iostream>
+using namespace std;
+int main(){
+    typedef unsigned int aec;
+    aec num1=10;
+    cout<<"Integer num1:"<<num1<<endl;
+
+    typedef unsigned long long ull;
+    ull bigNum=545413643545123;
+    cout<<"Ull bigNum"<<bigNum<<endl;
+
+return 0;
+}
